1b0b14857c683207d128d8ae08977a8100312d5b
Rohiin <mrohini216@gmail.com>
2021-08-31 23:31:00 +0530

    Input output processing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var btnTransulate=document.querySelector("#btn-transulate");
 var txtinput=document.querySelector("#txt-input");
+var outputdiv=document.querySelector("#output");
+
+console.log(outputdiv);
 
 
 btnTransulate.addEventListener("click", function clickEventHandler(){
-    console.log("Clicked!");
-    console.log("Text entered is: "+txtinput.value);
+    //console.log("Clicked!");
+    //console.log("Text entered is: "+txtinput.value);
+    outputdiv.innerText="Transulated Text is: "+txtinput.value;
 })
