57bb2e66939af4ddb25c64f2cf5dbd4e00daeecc
Rohiin <mrohini216@gmail.com>
2021-09-01 23:04:28 +0530

    added fetch and that response

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ var btnTransulate=document.querySelector("#btn-transulate");
 var txtinput=document.querySelector("#txt-input");
 var outputdiv=document.querySelector("#output");
 
-console.log(outputdiv);
+var serverURL="https://api.funtranslations.com/translate/minion.json";
 
+function getTransulationURL(text){
+    retun serverURL+"?"+"text="+text
+}
+
+function errorHandeler(error){
+    console.log("Error Occured"+error);
+    alert("Something wrong with server! Try after some time!");
+}
 
 btnTransulate.addEventListener("click", function clickEventHandler(){
-    //console.log("Clicked!");
-    //console.log("Text entered is: "+txtinput.value);
-    outputdiv.innerText="Transulated Text is: "+txtinput.value;
-})
+    var inputText=txtinput.value;
+
+    fetch(getTransulationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var transulatedText=json.contents.transulated;
+            outputdiv.innerText=transulatedText;
+        })
+        .catch(errorHandeler)
+});
