f9fcaef6ef7831c844b3256a2a4329a26091ac54
Rohiin <mrohini216@gmail.com>
2021-09-01 23:28:21 +0530

    docs: translated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var outputdiv=document.querySelector("#output");
 var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 function getTransulationURL(text){
-    retun serverURL+"?"+"text="+text
+    return serverURL + "?" + "text=" + text
 }
 
 function errorHandeler(error){
@@ -16,11 +16,9 @@ function errorHandeler(error){
 btnTransulate.addEventListener("click", function clickEventHandler(){
     var inputText=txtinput.value;
 
-    fetch(getTransulationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            var transulatedText=json.contents.transulated;
-            outputdiv.innerText=transulatedText;
+    fetch(getTransulationURL(inputText)) .then(response => response.json()) .then(json => {
+            var translatedText=json.contents.translated;
+            outputdiv.innerText=translatedText;
         })
         .catch(errorHandeler)
 });
